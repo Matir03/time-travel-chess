c2feb4765653d4ce617525c6731f953b33c54e1d
Ritam Nag <rnag@mit.edu>
2022-06-08 10:42:13 +0530

    temp

--- frontend/src/commontypes.ts
+++ frontend/src/commontypes.ts
@@ -1,4 +1,4 @@
-import { Color, Key } from "chessground/types";
+import { Color, Key, Role } from "chessground/types";
 
 export type SeekColor = "White" | "Black" | "Random";
 export const colors: Array<SeekColor> = ["White", "Black", "Random"];
@@ -95,6 +95,7 @@ export class RemoveSeek implements LobbyEvent {
 export interface Move {
     orig: Key;
     dest: Key;
+    prom?: Role;
 }
 
 export interface GameState {
