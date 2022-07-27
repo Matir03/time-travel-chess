c6db242c795942296e03b1cc8961f333505ce43b
Ritam Nag <rnag@mit.edu>
2022-07-28 00:17:51 +0530

    cleanup

--- server/src/config.ts
+++
@@ -1,5 +0,0 @@
-export const HOST = 'localhost';
-export const SOCKET_PORT = 3000;
-export const HTTP_PORT = 8080;
-export const SOCKET_ADDR = HOST + ':' + SOCKET_PORT;
-export const HTTP_ADDR = HOST + ':' + HTTP_PORT;
